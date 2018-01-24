031b63b869245dbd0f8b2c5a2974321b20cab18e
Damir Davletov <damir.davletov@corp.badoo.com>
2018-01-24 11:41:30 +0000

    Renamed framework to SDCAlertView and added custom modulemap with umbrella header

--- SDCAlertView iOS/SDCAlertView_iOS.h
+++ SDCAlertView iOS/SDCAlertView_iOS.h
@@ -14,6 +14,27 @@ FOUNDATION_EXPORT double SDCAlertView_iOSVersionNumber;
 //! Project version string for SDCAlertView_iOS.
 FOUNDATION_EXPORT const unsigned char SDCAlertView_iOSVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <SDCAlertView_iOS/PublicHeader.h>
+#import <SDCAlertView_iOS/SDCAlertCollectionViewCell.h>
+#import <SDCAlertView_iOS/"SDCAlertController.h>
+#import <SDCAlertView_iOS/"SDCAlertControllerCollectionViewFlowLayout.h>
+#import <SDCAlertView_iOS/"SDCAlertControllerDefaultVisualStyle.h>
+#import <SDCAlertView_iOS/"SDCAlertControllerScrollView.h>
+#import <SDCAlertView_iOS/"SDCAlertControllerTextFieldViewController.h>
+#import <SDCAlertView_iOS/"SDCAlertControllerTransition.h>
+#import <SDCAlertView_iOS/"SDCAlertControllerView.h>
+#import <SDCAlertView_iOS/"SDCAlertControllerVisualStyle.h>
+#import <SDCAlertView_iOS/"SDCAlertLabel.h>
+#import <SDCAlertView_iOS/"SDCAlertPresentationController.h>
+#import <SDCAlertView_iOS/"SDCAlertTextFieldTableViewCell.h>
+#import <SDCAlertView_iOS/"SDCAlertView.h>
+#import <SDCAlertView_iOS/"SDCAlertViewBackgroundView.h>
+#import <SDCAlertView_iOS/"SDCAlertViewContentView.h>
+#import <SDCAlertView_iOS/"SDCAlertViewController.h>
+#import <SDCAlertView_iOS/"SDCAlertViewCoordinator.h>
+#import <SDCAlertView_iOS/"SDCAlertViewTransitioning.h>
+#import <SDCAlertView_iOS/"SDCDemoViewController.h>
+#import <SDCAlertView_iOS/"SDCIntrinsicallySizedView.h>
+#import <SDCAlertView_iOS/"UIView+Parallax.h>
+#import <SDCAlertView_iOS/"UIViewController+Current.h>
 
 
